11c8c7d407c98548b364acee3ea60eafee8697ee
unknown <nikkalyan52@gmail.com>
2024-06-14 17:59:31 +0530

    mini and card hover added

--- Bootstrap-Cisco-Crusaders_092/script.js
+++ Bootstrap-Cisco-Crusaders_092/script.js
@@ -5,25 +5,4 @@ src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
-// const path = require('path');
 
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist')
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader']
-//       },
-//       {
-//         test: /\.(js)$/,
-//         exclude: /node_modules/,
-//         use: ['babel-loader']
-//       }
-//     ]
-//   }
-// };
---
+++ Bootstrap-Cisco-Crusaders_092/tailwind.config.js
@@ -0,0 +1,12 @@
+// /** @type {import('tailwindcss').Config} */
+// // tailwind.config.js
+// module.exports = {
+//   prefix: 'tw-',  // Add this line
+//   content: [
+//     './src/**/*.{html,js}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
